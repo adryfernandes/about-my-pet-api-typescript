5d89963d3fbe792025565dbdb377c127c27631d3
adryfernandes <adryane.fernandes.dev@gmail.com>
2023-11-27 22:27:44 -0300

    Configurando importação e ajeitando arquivos

--- src/types/paginate.type.ts
+++ src/types/paginate.type.ts
@@ -1,4 +1,4 @@
-import type { Timestamp } from '@/database/entities/extendings/timestamp';
+import type { Timestamp } from '@/database/entities/extendings/Timestamp';
 
 export type EntityDefault<Entity> = {
   timestamp: Timestamp;
